0ad07d6bc60c2ae842e196f9936eff91e2443f92
Marcelo Rodrigues <ruysmarcelo@gmail.com>
2024-03-02 21:24:24 -0300

    Add Alembic ORM

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 
-from shared.database import Base, engine
 from contas_a_pagar_e_receber.routers import contas_a_pagar_e_receber_router
-
-from contas_a_pagar_e_receber.models.conta_a_pagar_e_receber_model import ContaPagarReceber
-
-# Base.metadata.drop_all(bind=engine)
-# Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
